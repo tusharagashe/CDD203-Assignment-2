dcc6e8a29dc29582e9f022d892107cef29410678
tusharagashe <tusharagashe1@gmail.com>
2024-11-07 03:01:39 -0800

    modify init file

--- regression/__init__.py
+++ regression/__init__.py
@@ -4,5 +4,12 @@ CDD 203: Foundations to Machine Learning 2024
 Extracted from: 
     BMI203: Biocomputing algorithms Winter 2022
 """
+from .logreg import (
+    LogisticRegression
+)
+
+from .utils import (
+    loadDataset
+)
 
 __version__ = "0.1.1"
