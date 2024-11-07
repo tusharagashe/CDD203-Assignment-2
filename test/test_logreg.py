33ba5fa5c89ecf3bc1615f7052b3fd355b1900e0
tusharagashe <tusharagashe1@gmail.com>
2024-11-07 03:19:27 -0800

    add comments

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -21,37 +21,38 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_val = sc.transform (X_val)
 
+X_train_ones = np.ones((3,3))
+y_train_zeros = np.zeros((3,3))
+
+#validate gradient calculation is correct 
 def test_gradient_calculation():
-	X_train_ones = np.ones((3,3))
-	y_train_zeros = np.zeros((3,3))
 	log_model = logreg.LogisticRegression(num_feats=2, max_iter=10, tol=0.01, learning_rate=0.001, batch_size=12)
 	log_model.W = np.array([0.5,0.5,0.5])
 	grad = log_model.calculate_gradient(X_train_ones, y_train_zeros)
 	assert np.allclose(grad, np.full((3, 3), 2.452723428580931))
  
+#check if model can avoid exploding gradiennt with high learning rate
 def test_exploding_gradient():
 	log_model = logreg.LogisticRegression(num_feats=6, max_iter=10, tol=0.001, learning_rate=0.1, batch_size=100)
-
 	log_model.train_model(X_train, y_train, X_val, y_val)
 	relative_changes = np.abs(np.diff(log_model.loss_history_train) / log_model.loss_history_train[:-1])
-    
   # Check if any relative change exceeds the threshold
 	assert np.all(relative_changes <= 1.5)
  
+#check if model can avoid vanishing gradiennt with low learnning rate 
 def test_vanishing_gradient():
 	log_model = logreg.LogisticRegression(num_feats=6, max_iter=10, tol=0.00001, learning_rate=0.0001, batch_size=12)
-
 	log_model.train_model(X_train, y_train, X_val, y_val)
 	relative_changes = np.abs(np.diff(log_model.loss_history_train) / log_model.loss_history_train[:-1])
-
-	# Check if any relative change exceeds the threshold
 	assert np.all(relative_changes >= 1e-5)
 
+#check if loss values are decreasing generally over iterationns  
 def test_loss_decreasing():
   log_model = logreg.LogisticRegression(num_feats=6, max_iter=10, tol=0.001, learning_rate=0.01, batch_size=100)
   log_model.train_model(X_train, y_train, X_val, y_val)
   assert np.all(np.diff(log_model.loss_history_train)) 
   
+#check if the loss values in the final iterations are converging and not changing drastically 
 def test_loss_converging():
   log_model = logreg.LogisticRegression(num_feats=6, max_iter=100, tol=0.001, learning_rate=0.01, batch_size=100)
   log_model.train_model(X_train, y_train, X_val, y_val)
@@ -59,19 +60,20 @@ def test_loss_converging():
   #check if the loss in the last 10 iterations is varying by lessthan max allowed to change meaning more stability and convergence occuring 
   assert np.all(np.abs(np.diff(log_model.loss_history_train[-10:])) <= max_allowed_change) 
 
+#checks if weights are being changed 
 def test_weight_change():
   log_model = logreg.LogisticRegression(num_feats=6, max_iter=100, tol=0.001, learning_rate=0.01, batch_size=100)
   old_weights = log_model.W
   log_model.train_model(X_train, y_train, X_val, y_val)
   assert not np.array_equal(old_weights, log_model.W)
 
+#checks if make prediciton function is correctly returning values between 0 and 1 and at the same input shape  
 def test_make_prediction():
-  X_train_ones = np.ones((3,3))
-  y_train_zeros = np.zeros((3,3))
   log_model = logreg.LogisticRegression(num_feats=2, max_iter=10, tol=0.01, learning_rate=0.001, batch_size=12)
   y_pred = log_model.make_prediction(X_train_ones)
   assert np.all(y_pred >= 0) and np.all(y_pred <= 1) and y_pred.shape[0] == X_train_ones.shape[0]
  
+#checks model accuracy meets 50% or greater 
 def test_accuracy_post_training():
   log_model = logreg.LogisticRegression(num_feats=6, max_iter=100, tol=0.001, learning_rate=0.01, batch_size=100)
   log_model.train_model(X_train, y_train, X_val, y_val)
